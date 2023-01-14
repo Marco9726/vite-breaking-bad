491062bef3042eb63d6d32e806b1cddd9b8a3749
Marco 9726 <marcosarra9@gmail.com>
2023-01-14 17:07:00 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+  export default {
+    
+  }
+</script>
+
+<template lang="">
+  <div>
+    
+  </div>
+</template>
+
+<style lang="scss">
+  @use './scss/generals.scss' as *;
+  
+</style>
